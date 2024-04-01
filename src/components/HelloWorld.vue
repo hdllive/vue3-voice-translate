23045c18b8b4e0e9670837a363f95b55078c0f30
胡扬名 <hdllive@163.com>
2024-04-01 11:49:28 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup>
+import VoiceTranslate from './VoiceTranslate.vue'
+</script>
+
+<template>
+  <div class="card">
+    <voice-translate></voice-translate>
+  </div>
+</template>
+
+<style scoped>
+</style>
